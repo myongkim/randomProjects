1634958b4a2e55c27325dcf24c3f72e24196793d
Isaac Kim <myong328@gmail.com>
2020-01-15 15:05:22 -0800

    initial project

---
+++ pitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pitchPerfect
+//
+//  Created by Isaac Kim on 14/01/2020.
+//  Copyright © 2020 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func recordAudio(_ sender: UIButton) {
+        print("record button was pressed")
+        
+        
+    }
+    
+}
+
